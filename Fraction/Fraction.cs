9ac08db4d8f0c883c4888e221060db7759c36469
Kiryl Siaukovich <k.sevkovich@gmail.com>
2017-07-10 01:50:22 -0400

    Add files via upload

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace FractionTest
+{
+    class Fraction : IEquatable<Fraction>, IComparable<Fraction>, IComparable
+    {
+        #region PROPERTIES
+        private int Numerator { get; }
+        private int Denomenator { get; }
+        #endregion
+
+        #region CONSTRUCTORS
+        // Ctor that creates 1/1 fraction.
+        public Fraction()
+        {
+            Numerator = Denomenator = 1;
+        }
+
+        // Ctor that creates pNumerator / pDenomenator fraction.
+        public Fraction(int pNumerator, int pDenominator)
+        {
+            if (pDenominator == 0)
+                throw new DivideByZeroException("Denominator can not be 0!");
+
+            var gcd = Gcd(Math.Abs(pNumerator), Math.Abs(pDenominator));
+
+            if (pDenominator < 0)
+            {
+                pNumerator *= -1;
+                pDenominator *= -1;
+            }
+
+            Numerator = pNumerator / gcd;
+            Denomenator = pDenominator / gcd;
+        }
+        #endregion
+
+        #region OPERATORS_OVERLOAD
+        // Addition of fractions.
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            var newDenomenator = f1.Denomenator * f2.Denomenator;
+            var newNumerator   = f2.Denomenator * f1.Numerator + 
+                                 f1.Denomenator * f2.Numerator;
+          
+            return new Fraction(newNumerator, newDenomenator);
+        }
+
+        // Substraction of fractions.
+        // Add this fraction to negative other.
+        public static Fraction operator -(Fraction f1, Fraction f2) => 
+            f1 + new Fraction(f2.Numerator * -1, f2.Denomenator);
+
+        // Product of fractions.
+        public static Fraction operator *(Fraction f1, Fraction f2) => 
+            new Fraction(f1.Numerator * f2.Numerator, f1.Denomenator * f2.Denomenator);
+
+        // Quotient of fractions.
+        // Reverse the second fraction and multiply.
+        public static Fraction operator /(Fraction f1, Fraction f2) => 
+            f1 * new Fraction(f2.Denomenator, f2.Numerator);
+        #endregion
+
+        #region COMPARISON _OPERATORS
+        public static bool operator ==(Fraction f1, Fraction f2) =>
+            ReferenceEquals(f1, null) ? ReferenceEquals(f2, null) : f1.Equals(f2);
+
+        public static bool operator !=(Fraction f1, Fraction f2) => 
+            !(f1 == f2);
+
+
+        // Multiply both fractions in inequality by f1.Denomenator * f2.Denomenator.
+        public static bool operator >(Fraction f1, Fraction f2) => 
+            f1.Numerator * f2.Denomenator > f2.Numerator * f1.Denomenator;
+
+        public static bool operator <(Fraction f1, Fraction f2) =>
+            !(f1 > f2) && !(f1 == f2);
+        #endregion
+
+        #region OBJECTS_METHODS
+        // Overrides the Object's three methods.
+        public override string ToString()
+        {
+            if (Numerator == 0)
+                return "0";
+
+            if (Denomenator == 1)
+                return Numerator.ToString();
+
+            return $"{Numerator}/{Denomenator}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var f = obj as Fraction;
+            
+            return f != null && 
+                this.Numerator   == f.Numerator && 
+                this.Denomenator == f.Denomenator;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Numerator * 397) ^ Denomenator;
+            }
+        }
+        #endregion
+
+        #region INTERFACES_IMPLEMENTATION
+        // IComparable implementation.
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Fraction))
+                throw new ArgumentException("Argument type should be Fraction");
+
+            return CompareTo(obj as Fraction);
+        }
+
+        // IComparable<Fraction> implementation.
+        public int CompareTo(Fraction other) =>
+            (this < other) ? -1 :
+            (this > other) ?  1 : 0;
+
+
+        // IEquitable implementation.
+        public bool Equals(Fraction other)
+        {
+            if (ReferenceEquals(other, null))
+                return false;
+
+            return this.Numerator   == other.Numerator 
+                && this.Denomenator == other.Denomenator;
+        }
+        #endregion
+
+        #region CAST_OPERATORS
+        // Cast operators overload.
+        public static explicit operator int(Fraction f) => 
+            f.Numerator / f.Denomenator;
+
+        public static explicit operator double(Fraction f) => 
+            Convert.ToDouble(f.Numerator) / f.Denomenator;
+        #endregion
+
+        #region OTHER
+        // Greatest common divisior.
+        private static int Gcd(int q, int r)
+        {
+            var max = q >= r ? q : r;
+            var min = q < r ? q : r;
+
+            while (true)
+            {
+                if (min == 0)
+                    return max;
+
+                var max1 = max;
+                max = min;
+                min = max1 % min;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Fraction/WorkingWithFraction.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Text;
+
+namespace FractionTest
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
+
+            Console.WriteLine("Input first fraction.");
+            var fraction1 = GetFraction();
+
+            Console.WriteLine("Input second fraction.");
+            var fraction2 = GetFraction();
+
+            Console.WriteLine("Fractions (irreducible):\n" + fraction1 + '\n' + fraction2 + '\n');
+
+            // fraction1 + fraction2
+            Console.WriteLine("Sum:\n(" + fraction1 + ") + (" + fraction2 + ") = " + (fraction1 + fraction2) + '\n');
+
+            // fraction1 - fraction2
+            Console.WriteLine("Difference:\n(" + fraction1 + ") - (" + fraction2 + ") = " + (fraction1 - fraction2));
+
+            // fraction2 - fraction1
+            Console.WriteLine("(" + fraction2 + ") - (" + fraction1 + ") = " + (fraction2 - fraction1) + '\n');
+
+            // fraction1 * fraction2
+            Console.WriteLine("Product:\n(" + fraction1 + ") * (" + fraction2 + ") = " + (fraction1 * fraction2) + '\n');
+
+            // fraction1 / fraction2
+            try
+            {
+                Console.WriteLine("Quotient:\n(" + fraction1 + ") / (" + fraction2 + ") = " + (fraction1/fraction2));
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Quotient: Error: second fraction is 0!");
+            }
+
+            // fraction2 / fraction1
+            try
+            {
+                Console.WriteLine("(" + fraction2 + ") / (" + fraction1 + ") = " + (fraction2/fraction1) + '\n');
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Quotient: Error: first fraction is 0!\n");
+            }
+
+            var relation = (fraction1.Equals(fraction2) ? " == " : " != ");
+            Console.WriteLine("Equality: (" + fraction1 + ")" + relation + "(" + fraction2 + ")");
+
+            switch (fraction1.CompareTo(fraction2))
+            {
+                case -1:
+                    relation = " < ";
+                    break;
+                case 1:
+                    relation = " > ";
+                    break;
+            }
+
+            Console.WriteLine("More or less: (" + fraction1 + ")" + relation + "(" + fraction2 + ")");
+            Console.WriteLine("\nCasted to int:\n(int)" + fraction1 + " = " + ((int)fraction1) + "\n(int)" + fraction2 + " = " + ((int)fraction2));
+            Console.WriteLine("\nCasted to double:\n(double)" + fraction1 + " = " + ((double)fraction1) + "\n(double)" + fraction2 + " = " + ((double)fraction2));
+            Console.WriteLine();
+
+            Fraction[] arrayFractions;
+            var rng = new Random();
+            while (true)
+            {
+                try
+                {
+                    arrayFractions = new[]
+                    {
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                        new Fraction(rng.Next(-100, 100), rng.Next(-100, 100)),
+                    };
+
+                    break;
+                }
+                catch (DivideByZeroException)
+                {
+                    // In case if one of the fractions in array would have 0 as denominator.
+                }
+            }
+            Console.WriteLine("Array of random fractions (unsorted): ");
+            foreach (var fraction in arrayFractions)
+                Console.Write(fraction + " | ");
+
+            Console.WriteLine();
+
+            Array.Sort(arrayFractions);
+            Console.WriteLine("Array of random fractions (sorted): ");
+            foreach (var fraction in arrayFractions)
+                Console.Write(fraction + " | ");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any to exit.");
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        static Fraction GetFraction()
+        {
+            Console.Write("Input numerator: ");
+            int numerator;
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out numerator))
+                    break;
+
+                Console.Write("Wrong input, try again: ");
+            }
+            Console.Write("Input denominator: ");
+
+            while (true)
+            {
+                int denominator;
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out denominator))
+                {
+                    try
+                    {
+                        return new Fraction(numerator, denominator);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Wrong input, denominator can not be 0!\n" +
+                                          "Try again: ");
+                    }
+                }
+            }
+
+        }
+
+    }
+}
